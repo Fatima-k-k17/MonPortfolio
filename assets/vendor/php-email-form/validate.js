1b93bacb9a4b0755016a5c3315bb3011438df74d
Fatima-k-k17 <kontao123@icloud.com>
2026-06-19 08:36:05 +0000

    Activation du formulaire de contact avec FormSubmit pour kontao123@icloud.com

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -104,6 +104,12 @@
       this_form.find('.error-message').slideDown().html('The form action property is not set!');
       return false;
     }
+
+    if (action.indexOf('formsubmit.co') !== -1) {
+      this_form.find('.loading').slideDown();
+      this_form.get(0).submit();
+      return true;
+    }
     
     this_form.find('.sent-message').slideUp();
     this_form.find('.error-message').slideUp();
